20ef715da4958665bf0358b8f61f704cee30ac2b
GRAEME-PC\Graeme <morrisongraeme@hotmail.com>
2017-09-09 14:43:05 +0100

    Tweaking hyper-parameters for lunar lander

--- run_dqn.py
+++ run_dqn.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 
 # Generic settings
 render_interval = 100
-max_episodes = 4000
+max_episodes = 1000
 n_learning_repeats = 1
 start_with_filled_memory = True
 
 # Environment selection: 1 for MountainCar-v0, 2 for CartPole-v0, 3 for LunarLander-v2
-env_name = 'MountainCar-v0'
+# env_name = 'MountainCar-v0'
 # env_name = 'CartPole-v0'
-# env_name = 'LunarLander-v2'
+env_name = 'LunarLander-v2'
 
 # Make selected environment
 env = gym.make(env_name)
 
 # Environment-specific settings
 if env_name is 'MountainCar-v0':
-    # env._max_episode_steps = 100000  # Over-ride default maximum number of time steps allowed per episode
+    env._max_episode_steps = 100000  # Over-ride default maximum number of time steps allowed per episode
     n_states = env.observation_space.shape[0]
     n_actions = env.action_space.n-1  # Set this to n-1 because we are remapping the action space from size 3 to 2
     options = {'min_explore': 0.1, 'action_remap_gain': 2, 'separate_target': True, 'update_target_interval': 5000}
@@ -32,8 +32,9 @@ elif env_name is 'CartPole-v0':
 elif env_name is 'LunarLander-v2':
     # env._max_episode_steps = 200  # Over-ride default maximum number of time steps allowed per episode
     n_states = env.observation_space.shape[0]
-    n_actions = env.action_space.n - 1  # Set this to n-1 because we are remapping the action space from size 3 to 2
-    options = {'min_explore': 0.1}
+    n_actions = env.action_space.n
+    options = {'min_explore': 0.01, 'decay_explore': 0.0001, 'n_hidden_neurons': 256, 'separate_target': True,
+               'update_target_interval': 5000}
 
 # Initialise empty arrays to store rewards and explore rates per learning repeat
 reward_array = np.zeros((n_learning_repeats, max_episodes))
